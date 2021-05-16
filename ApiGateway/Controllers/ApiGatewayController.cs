dbea8a1424aff947a89c409797d7f18606a74b17
soufiene <soufiene.katet@outlook.com>
2021-05-16 12:00:56 +0200

    first commit

---
+++ ApiGateway/Controllers/ApiGatewayController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiGatewayController : ControllerBase
+    {
+        private readonly ILogger<ApiGatewayController> _logger;
+
+        public ApiGatewayController(ILogger<ApiGatewayController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "ApiGateway Up";
+        }
+    }
+}
